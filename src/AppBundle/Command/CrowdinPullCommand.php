1663dc9057fb2fb51fbba01abb6b85c3cda1dfec
Edgars Polis <edgras.polis@gmail.com>
2015-02-23 16:33:34 +0200

    Add Crowdin commands to sync translations

---
+++ src/AppBundle/Command/CrowdinPullCommand.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace AppBundle\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\Yaml\Dumper;
+
+class CrowdinPullCommand extends ContainerAwareCommand
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('crowdin:pull')
+            ->setDescription('Crowdin pull command')
+            ->setHelp(<<<EOF
+Pulls translations from Crowdin.
+EOF
+            );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('Setting up configurations');
+
+        // $finder = new Finder();
+        // $files = $finder->files()->in(__DIR__.'/../../../src')->name('*en.yml');
+
+        $key = $this->getContainer()->getParameter('crowdin_key');
+
+        $config = [
+            'project_identifier' => 'ebayumelis',
+            'api_key' => $key,
+            'base_path' => 'src',
+            'preserve_hierarchy' => true,
+            'files' => []
+        ];
+
+        $config['files'][] = [
+            'source' => '/**/*en.yml',
+            'translation' => '/**/messages.%two_letters_code%.yml'
+        ];
+
+        $dumper = new Dumper();
+        $yaml = $dumper->dump($config);
+
+        file_put_contents(__DIR__.'/../../../crowdin.yaml', $yaml);
+        $output->writeln('Pulling from Crowdin');
+        $command = 'java -jar app/Resources/Crowdin/crowdin-cli.jar download';
+        exec($command);
+    }
+}
